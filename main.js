be531ff363c3d0b668c643ecd6c2d3c13c74a197
R4Y <103247254+AndriyDabucol@users.noreply.github.com>
2024-09-02 01:56:10 +0800

    With Re:Write Feature

--- main.js
+++ main.js
@@ -10,8 +10,13 @@ const noteTitle = document.getElementById("note-title");
 
 const notesDiv = document.getElementById("notes");
 
+const ReWriteBtn = document.getElementById("re-write-btn");
+const viewPreviousBtn = document.getElementById("view-previous-btn");
+
 
 let notes = []
+let isShowingPrev = false;
+
 
 notes = localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")) : [];
 
@@ -19,9 +24,19 @@ notes = localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")
 
 const accessNote = (index) => {
 
-
     noteTitle.value = '';
     noteContent.value = '';
+
+    viewPreviousBtn.style.display = "none";
+
+    NoteBtn1.value = null;
+    ReWriteBtn.value = null;
+
+    viewPreviousBtn.value = null;
+
+
+    ReWriteBtn.disabled = true;
+    ReWriteBtn.style.display = "none";
 
     console.log(index)
     createNoteModal.style.display = "flex"
@@ -34,22 +49,34 @@ const accessNote = (index) => {
         return;
     }
 
-    NoteBtn1.innerText = ' Edit ';
+    NoteBtn1.innerText = ' Save ';
     NoteBtn2.innerText = ' Close ';
 
+    ReWriteBtn.disabled = false;
+    ReWriteBtn.style.display = "inline";
+
     NoteBtn1.value = index;
+    ReWriteBtn.value = index;
+
+
+    viewPreviousBtn.style.display = notes[index].history.length > 0 ? "inline" : "none";
+    
+    viewPreviousBtn.value = index;
 
     noteTitle.value = notes[index].title;
     noteContent.value = notes[index].text;
 }
 
 const displayNotes = () => {
+    console.log("I fired")
     notes.forEach((note, index)=> {
         notesDiv.innerHTML += `
             <button class="note button" onclick="accessNote(${index})" id="${note.id}" value=${index} >
+                <h2></h2>
                 <h2>${note.title}</h2>
     
                 <p >${note.text.length > 40 ? `${note.text.substring(0, 40)}...` : note.text}</p>
+                
             </button>
         `
     
@@ -58,10 +85,10 @@ const displayNotes = () => {
 
 
 
-
 const finishNote = (e) => {
+    notesDiv.innerHTML = ``
 
-    notesDiv.innerHTML = ``    
+    index = Number(e.target.value); 
 
     if (noteTitle.value === "" && noteContent.value === ""){
         alert("Please enter a title and/or enter text.");
@@ -72,12 +99,15 @@ const finishNote = (e) => {
     createNoteModal.style.display = "none";
     const noteID = `${noteTitle.value.toLowerCase().split(" ").join("-")}-${notes.length}`;
 
-    if(e.target.value){
-        editNote(e.target.value, noteID);
+    noteContent.placeholder = "Take down some notes, make sure to keep it short!";
+
+    if(notes[index]){
+        console.log(notes[index])
+        editNote(index, noteID);
         return;
     }
 
-    notes.push({title: noteTitle.value, text: noteContent.value, time: 5, id: noteID});
+    notes.push({title: noteTitle.value, text: noteContent.value, time: 0, id: noteID, index: e.target.value, history: []});
     localStorage.setItem("notes", JSON.stringify(notes))
 
     noteTitle.value = '';
@@ -87,21 +117,109 @@ const finishNote = (e) => {
 }
 
 const editNote = (index, ID) => {
-    notes[index] = {title: noteTitle.value, text: noteContent.value, time: 5, id: ID};
+    isShowingPrev = false;
+    notes[index] = {title: noteTitle.value, text: noteContent.value, time: 0, id: ID, index: index, history: notes[index].history};
     localStorage.setItem("notes", JSON.stringify(notes))
     noteTitle.value = '';
     noteContent.value = '';
     displayNotes();
 }
 
+const reWriteNote = (e) => {
+    notesDiv.innerHTML = ``
 
+    const index = e.target.value;
+
+    console.log(index)
+    let count = 60;
+
+    notes[index].history.unshift({ 
+        title: notes[index].title, 
+        text: notes[index].text,
+        id: notes[index].id,
+        index: notes[index].index
+        }
+    )
+
+    editNote(index, notes[index].id)
+    
+
+    const displayedNote = document.getElementById(notes[index].id)
+
+    noteContent.placeholder = `Re:Write your notes! 
+    
+    Rewrite your old notes to the best of your ability. Only use the "View Previous" if you're really stuck or once you're complete with your re-write!
+
+    Hint: ${notes[index].text.substring(0, 40)}...`;
+
+    notes[index].text = ""
+
+    localStorage.setItem("notes", JSON.stringify(notes))
+
+    createNoteModal.style.display = "none";
+
+    displayedNote.disabled = true;
+
+    const timer = setInterval(()=>{
+        displayedNote.firstElementChild.innerText = count;
+        count -= 1;
+        if (count < 0){
+            clearInterval(timer);
+            displayedNote.disabled = false;
+            displayedNote.firstElementChild.innerText = "";
+        }
+    }, 1000);
+
+}
+
+
+let current = {};
+
+
+
+const viewPrevious = (e) => {
+    
+    isShowingPrev = !isShowingPrev;
+
+    const index = e.target.value
+
+    if(isShowingPrev){
+        current = {title: noteTitle.value, text: noteContent.value};
+        noteTitle.value = notes[index].history[0].title;
+        noteContent.value = notes[index].history[0].text;
+
+        viewPreviousBtn.innerText = "View Current";
+
+        noteContent.disabled = true;
+        NoteBtn1.disabled = true;
+        NoteBtn2.disabled = true;
+        ReWriteBtn.disabled = true;
+    }else{
+        noteContent.disabled = false;
+        NoteBtn1.disabled = false;
+        NoteBtn2.disabled = false;
+        ReWriteBtn.disabled = false;
+        noteTitle.value = current.title
+        noteContent.value = current.text
+
+        viewPreviousBtn.innerText = "View Previous";
+        
+    }
+    
+}
 
 
 newNoteBtn.addEventListener('click', accessNote)
 NoteBtn1.addEventListener('click', finishNote)
-NoteBtn2.addEventListener('click', ()=>{createNoteModal.style.display = "none";})
+NoteBtn2.addEventListener('click', ()=>{
+    createNoteModal.style.display = "none";
+    isShowingPrev = false;
+    noteContent.placeholder = "Take down some notes, make sure to keep it short!";
+})
 
+ReWriteBtn.addEventListener('click', reWriteNote)
+viewPreviousBtn.addEventListener('click', viewPrevious)
 
-
+//localStorage.setItem("notes", []);
 
 displayNotes();
